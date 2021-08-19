0ecc1f8c89fb535375efc17a8059ada1e5353054
Jonas Boone <jonas.boone@student.kdg.be>
2021-08-19 17:28:32 +0200

    Added React portals to render overlay and backdrop elsewhere in the DOM

--- src/components/UI/ErrorModal.tsx
+++ src/components/UI/ErrorModal.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Button from "./Button";
 import styles from "./ErrorModal.module.css";
 
@@ -8,24 +9,31 @@ interface IProp {
     onConfirm: () => void
 }
 
+const Backdrop = (props: any) => {
+    return <div className={styles["backdrop"]} onClick={props.onConfirm}/>;
+}
+
+const ModalOverlay = (props: IProp) => {
+    return <div className={styles["modal"]}>
+        <header className={styles["header"]}>
+            <h2>{props.title}</h2>
+        </header>
+        <div className={styles["content"]}>
+            <p>{props.message}</p>
+        </div>
+        <footer className={styles["actions"]}>
+            <Button onClick={props.onConfirm}>Got it!</Button>
+        </footer>
+    </div>
+}
+
 const ErrorModal = (props: IProp) => {
 
     return (
         <React.Fragment>
-            <div className={styles["backdrop"]} onClick={props.onConfirm}/>
-            <div className={styles["modal"]}>
-                <header className={styles["header"]}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={styles["content"]}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={styles["actions"]}>
-                    <Button onClick={props.onConfirm}>Got it!</Button>
-                </footer>
-            </div>
+            { ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm}/>, document.getElementById("backdrop-root")! as HTMLDivElement)}
+            { ReactDOM.createPortal(<ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm}/>, document.getElementById("overlay-root")! as HTMLDivElement)}
         </React.Fragment>
-
     )
 }
 
